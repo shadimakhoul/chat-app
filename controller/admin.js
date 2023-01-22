26dff21e781f978c65da84d56cc5b0ff95d1c728
shadi makhoul <shadimakhoul570@gmail.com>
2023-01-22 23:32:54 +0200

    first push

--- controller/admin.js
+++ controller/admin.js
@@ -1,10 +1,10 @@
 module.exports = (app, query) => {
-    // app.get("/chat-app/admin/get-chats", (req, res) => {
-    //     query("SELECT ch.id, ch.user1, ch.user2, us.user_name, sev.user_name from chats ch INNER join users us on ch.user1 = us.id INNER JOIN ( SELECT users.user_name, chats.user1, chats.user2, chats.id FROM chats INNER JOIN users on chats.user2 = users.id) sev on sev.id = ch.id", (err, result) => {
-    //         if (err){
-    //             return res.status(500).json(err.message)
-    //         }
-    //         return res.json(result)
-    //     })
-    // })
+    app.get("/chat-app/admin/get-chats", (req, res) => {
+        query("SELECT ch.id, ch.user1, ch.user2, us.user_name, sev.user_name from chats ch INNER join users us on ch.user1 = us.id INNER JOIN ( SELECT users.user_name, chats.user1, chats.user2, chats.id FROM chats INNER JOIN users on chats.user2 = users.id) sev on sev.id = ch.id", (err, result) => {
+            if (err){
+                return res.status(500).json(err.message)
+            }
+            return res.json(result)
+        })
+    })
 }
